14ee016df60754191d44bea3cab0792c88373b12
sbajra <senad.bajra@gmail.com>
2024-08-26 15:00:13 +0200

    implemented local storage, not completed

--- src/globalState/itemStore.js
+++ src/globalState/itemStore.js
@@ -1,34 +1,21 @@
-import zustymiddleware from "zustymiddleware";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 const useItemStore = create(
-  zustymiddleware(
+  persist(
     (set) => ({
-      // state
+      items: [],
 
-      items: [
-        { id: 1, itemName: "Banana", unitPrice: 7, type: "food" },
-        { id: 2, itemName: "Apple", unitPrice: 20, type: "food" },
-        { id: 3, itemName: "Orange", unitPrice: 4, type: "food" },
-        { id: 4, itemName: "Pear", unitPrice: 4, type: "food" },
-        { id: 5, itemName: "Espresso", unitPrice: 4, type: "drink" },
-      ],
-
-      // actions
-
-      //createNewItem
       addNewItem: (newItem) => {
         set((state) => ({
           items: [...state.items, newItem],
         }));
       },
-      //deleteItems
       removeItem: (id) => {
         set((state) => ({
           items: state.items.filter((item) => item.id !== id),
         }));
       },
-      //update
       updateItem: (updateItem) => {
         set((state) => ({
           items: state.items.map((item) =>
@@ -38,10 +25,10 @@ const useItemStore = create(
       },
     }),
     {
-      name: "item-storage", // key in localStorage
+      name: "item-storage",
+      getStorage: () => localStorage,
     },
   ),
 );
 
-window.store = useItemStore;
 export default useItemStore;
